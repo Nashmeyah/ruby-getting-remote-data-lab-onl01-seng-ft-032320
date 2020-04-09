5cfecf019dd5cf6a3eb4e5473a460e23293f9cdc
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-09 22:23:47 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,16 +1,17 @@
 class GetRequester
-
-  def initialize(url)
-    @url = url
-  end
-
-  def get_response_body
-    uri = URI.parse(@url)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
-
-  def parse_json
-    programs = JSON.parse(self.get_response_body)
-  end
+#@URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+def initialize(url)
+@url = url
+end
+def get_response_body
+uri = URI.parse(@url)
+response = Net::HTTP.get_response(uri)
+response.body
+end
+def parse_json
+programs = JSON.parse(self.get_response_body)
+#programs.collect do |program|
+#program["agency"]
+#end
+end
 end
